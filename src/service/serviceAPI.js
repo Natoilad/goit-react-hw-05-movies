0516a82a4f8534502c4a550cfd0b67ab9e22f32c
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-03-17 02:31:04 +0200

    update fetch

--- src/service/serviceAPI.js
+++ src/service/serviceAPI.js
@@ -1,52 +1,59 @@
 import axios from 'axios';
-import { response } from 'express';
-import { async } from 'q';
+
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = '178b07d62e47484c1918df64bd242568';
 
 export const fetchSearchMovies = async query => {
   try {
-    const response = await axios.get(
+    const resp = await axios.get(
       `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=1&language=en-US&include_adult=false`
     );
-    return response.data;
+    return resp.data;
   } catch (error) {
-    throw new Error(response.statusText);
+    throw new Error(error.statusText);
   }
 };
 
 export const fetchTrending = async () => {
   try {
-    const response = await axios.get(
+    const resp = await axios.get(
       `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=1`
     );
+    return resp.data;
   } catch (error) {
-    throw new Error(response.statusText);
+    throw new Error(error.statusText);
   }
 };
+
 export const fetchDetailsMovie = async id => {
   try {
-    const response = await axios.get(
-      `${BASE_URL}/movie/${id}?api_key=${API_KEY}&page=1&&language=en-US`
+    const resp = await axios.get(
+      `${BASE_URL}/movie/${id}?api_key=${API_KEY}&page=1&language=en-US`
     );
+    return resp.data;
   } catch (error) {
-    throw new Error(response.statusText);
+    throw new Error(error.statusText);
   }
 };
 
 export const fetchCast = async id => {
   try {
-    const response = await axios.get(
-      `${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}&page=1&&language=en-US`
+    const resp = await axios.get(
+      `${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
     );
+    return resp.data;
   } catch (error) {
-    throw new Error(response.statusText);
+    throw new Error(error.statusText);
   }
 };
 
 export const fetchReviews = async id => {
   try {
+    const resp = await axios.get(
+      `${BASE_URL}/movie/${id}/reviews?api_key=${API_KEY}&language=en-US`
+    );
+    return resp.data;
   } catch (error) {
-    throw new Error(response.statusText);
+    throw new Error(error.statusText);
   }
 };
