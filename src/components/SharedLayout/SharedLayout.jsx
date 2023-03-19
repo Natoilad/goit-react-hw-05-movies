ca0c3c727f251c4661941bb2654aacc682855b44
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-03-19 05:06:14 +0200

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Links } from './SharedLayout.styled';
 
 export const SharedLayout = () => {
